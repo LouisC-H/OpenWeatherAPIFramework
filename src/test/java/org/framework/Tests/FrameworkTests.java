6076fbbe00b74ad17a46bb3354f88c5bc8787b58
Pawel <pawel.dyjak@poczta.onet.pl>
2022-01-13 17:23:16 +0000

    Merge conflicts fixed

--- src/test/java/org/framework/Tests/FrameworkTests.java
+++ src/test/java/org/framework/Tests/FrameworkTests.java
@@ -1,8 +1,13 @@
-package org.framework.Tests;
+package org.framework.FrameworkTests;
+
 import org.framework.Injector;
+import org.framework.JsonTests.DTOValueChecker;
 import org.framework.connection_manager.ConnectionManager;
 import org.framework.openWeatherDTO.OpenWeatherDTO;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class FrameworkTests {
     private static OpenWeatherDTO openWeatherDTO;
@@ -13,5 +18,9 @@ public class FrameworkTests {
         openWeatherDTO = Injector.injectDTO(ConnectionManager.getURL());
     }
 
-
+    @Test
+    @DisplayName("stuff")
+    void stuff() {
+        Assertions.assertTrue(DTOValueChecker.checkTimezoneValue(openWeatherDTO));
+    }
 }
