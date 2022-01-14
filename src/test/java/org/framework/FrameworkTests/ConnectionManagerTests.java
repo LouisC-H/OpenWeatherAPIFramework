4d10515d163f109d2b6d9d840c8e0379aa98e0ce
TWParsons <TParsons@SpartaGlobal.com>
2022-01-14 14:17:25 +0000

    Duplicate and incorrect assertions removed

--- src/test/java/org/framework/FrameworkTests/ConnectionManagerTests.java
+++ src/test/java/org/framework/FrameworkTests/ConnectionManagerTests.java
@@ -31,9 +31,6 @@ public class ConnectionManagerTests {
     @DisplayName("Set Invalid City Name Test")
     void setInvalidCityNameTest(){
         ConnectionManager.setEndpointAsCityName("Disneyland");
-        Assertions.assertEquals(200, ConnectionManager.getStatusCode());
-        System.out.println(ConnectionManager.getURL());
-        ConnectionManager.setEndpointAsCityName("Disneyland");
         Assertions.assertEquals(404, ConnectionManager.getStatusCode());
     }
 
@@ -47,9 +44,6 @@ public class ConnectionManagerTests {
     @Test
     @DisplayName("Set Invalid State Code Test")
     void setInvalidStateCodeTest(){
-        ConnectionManager.setEndpointAsCityName("London", "es");
-        Assertions.assertEquals(200, ConnectionManager.getStatusCode());
-        System.out.println(ConnectionManager.getURL());
         ConnectionManager.setEndpointAsCityName("London", "es");
         Assertions.assertEquals(404, ConnectionManager.getStatusCode());
     }
@@ -78,7 +72,6 @@ public class ConnectionManagerTests {
     @Test
     @DisplayName("Set Invalid City ID Test")
     void setInvalidCityIdTest(){
-        ConnectionManager.setEndpointAsCityID(4206969);
         ConnectionManager.setEndpointAsCityID(4206969);
         Assertions.assertEquals(404, ConnectionManager.getStatusCode());
     }
